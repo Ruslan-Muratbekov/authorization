46dae93e7cb2f492bf851bec3900079363c06d09
Ruslan-Muratbekov <lepeshka2555@gmail.com>
2023-02-21 17:27:33 +0600

    validation body register

--- app/controller/user-controllers.js
+++ app/controller/user-controllers.js
@@ -1,9 +1,15 @@
 const UserSchema = require('../model/user-schema')
 const UserServices = require('../services/user-services')
+const {validationResult} = require('express-validator')
+const ApiError = require('../exceptions/api-error')
 
 class UserControllers {
 	async registration(req, res, next){
 		try {
+			const errors = validationResult(req)
+			if(!errors.isEmpty()){
+				return next(ApiError.BadRequest('Ошибка при валидации', errors.array()))
+			}
 			const {email, password} = req.body
 			const userData = await UserServices.registration(email, password)
 			res.cookie('refreshToken', userData.refreshToken , {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
--- app/router/index.js
+++ app/router/index.js
@@ -1,9 +1,14 @@
 const Router = require('express').Router
+const {body} = require('express-validator')
 const controller = require('../controller/user-controllers')
 
 const userRouter = new Router()
 
-userRouter.post('/registration', controller.registration)
+userRouter.post('/registration',
+	body('email').isEmail(),
+	body('password').isLength({min: 3, max: 32}),
+	controller.registration
+)
 userRouter.post('/login', controller.login)
 userRouter.post('/logout', controller.logout)
 userRouter.get('/activate/:link', controller.activate)
